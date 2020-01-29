1046fb8c1e808e40386ca2cd949fed23716f4ae4
Emilie Lindell <emilie.lindell@learnet.se>
2020-01-29 13:59:23 +0100

    Början
    Början av att göra klasserna och arven

---
+++ Emilie_lindell_prov/Superhjälte1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emilie_lindell_prov
+{
+    class Superhjälte1
+    {
+    }
+}
